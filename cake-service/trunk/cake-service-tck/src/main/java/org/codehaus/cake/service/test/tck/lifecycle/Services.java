281a176d614d5dae119d26e9e5b6d80d0ba9e4f6
kasper <kasper@3b99cd06-704a-0410-9335-aa0d59a2a4ad>
2008-08-09 13:04:56 +0000

    adding missing file

---
+++ cake-service/trunk/cake-service-tck/src/main/java/org/codehaus/cake/service/test/tck/lifecycle/Services.java
@@ -0,0 +1,97 @@
+package org.codehaus.cake.service.test.tck.lifecycle;
+
+import org.codehaus.cake.attribute.IntAttribute;
+import org.codehaus.cake.service.ServiceFactory;
+import org.codehaus.cake.service.ServiceRegistrant;
+import org.codehaus.cake.service.Startable;
+
+public class Services {
+
+    public static final IntAttribute I = new IntAttribute() {};
+
+    public static class RegisterKeyNull {
+        @Startable
+        public void start(ServiceRegistrant registrant) {
+            registrant.registerService(null, 0);
+        }
+    }
+
+    public static class RegisterServiceNull {
+        @Startable
+        public void start(ServiceRegistrant registrant) {
+            registrant.registerService(Integer.class, null);
+        }
+    }
+
+    public static class Register0 {
+        @Startable
+        public void start(ServiceRegistrant registrant) {
+            registrant.registerService(Integer.class, 0);
+        }
+    }
+
+    public static class Register1 {
+        @Startable
+        public void start(ServiceRegistrant registrant) {
+            registrant.registerService(Integer.class, 1);
+        }
+    }
+
+    public static class Register2 {
+        @Startable
+        public void start(ServiceRegistrant registrant) {
+            registrant.registerService(Integer.class, 2);
+        }
+    }
+
+    public static class RegisterFactory0 {
+
+        @Startable
+        public void start(ServiceRegistrant registrant) {
+            registrant.registerFactory(Integer.class, new ServiceFactoryStub(0));
+        }
+    }
+
+    public static class RegisterFactory1 {
+
+        @Startable
+        public void start(ServiceRegistrant registrant) {
+            registrant.registerFactory(Integer.class, new ServiceFactoryStub(1));
+        }
+    }
+
+    public static class RegisterFactory2 {
+
+        @Startable
+        public void start(ServiceRegistrant registrant) {
+            registrant.registerFactory(Integer.class, new ServiceFactoryStub(2));
+        }
+    }
+
+    public static class RegisterFactoryKeyNull {
+        @Startable
+        public void start(ServiceRegistrant registrant) {
+            registrant.registerFactory(null, new ServiceFactoryStub(1));
+        }
+    }
+
+    public static class RegisterFactoryServiceNull {
+        @Startable
+        public void start(ServiceRegistrant registrant) {
+            registrant.registerFactory(Integer.class, null);
+        }
+    }
+
+    static class ServiceFactoryStub implements ServiceFactory<Integer> {
+        private final int stride;
+
+        ServiceFactoryStub(int stride) {
+            this.stride = stride;
+        }
+
+        public Integer lookup(org.codehaus.cake.service.ServiceFactory.ServiceFactoryContext<Integer> context) {
+            int val = context.getAttributes().get(I) + stride;
+            return val >= 0 ? val : context.handleNext();
+        }
+    }
+}
