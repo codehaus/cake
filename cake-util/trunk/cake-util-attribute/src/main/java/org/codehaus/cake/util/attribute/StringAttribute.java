5dc05a9782e89419333ef5bc03542a74b017b85e
kasper <kasper@3b99cd06-704a-0410-9335-aa0d59a2a4ad>
2009-04-19 19:03:34 +0000

    Adding StringAttribute

---
+++ cake-util/trunk/cake-util-attribute/src/main/java/org/codehaus/cake/util/attribute/StringAttribute.java
@@ -0,0 +1,20 @@
+package org.codehaus.cake.util.attribute;
+
+public class StringAttribute extends ObjectAttribute<String> {
+
+    /** serialVersionUID */
+    private static final long serialVersionUID = 1L;
+
+    protected StringAttribute() {
+        super(String.class);
+    }
+
+    public StringAttribute(String defaultValue) {
+        super(String.class, defaultValue);
+    }
+
+    public StringAttribute(String name, String defaultValue) {
+        super(name, String.class, defaultValue);
+    }
+
+}
